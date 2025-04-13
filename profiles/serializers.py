982a8b230a2458a3209f613bbd0577eec8a920a0
almost-good <marja.marinic@gmail.com>
2025-04-14 00:28:21 +0100

    Include permissions to Profile detail CRUD

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -8,6 +8,11 @@ class ProfileSerializer(serializers.ModelSerializer):
     """
     
     owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
     
     class Meta:
         model = Profile
@@ -17,5 +22,6 @@ class ProfileSerializer(serializers.ModelSerializer):
             'created_at',
             'updated_at',
             'flair',
-            'image'
+            'image',
+            'is_owner',
         ]
