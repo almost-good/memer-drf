77c2edbf9cfb940e0010b154cfb068612ed3b9ad
almost-good <marja.marinic@gmail.com>
2025-04-13 22:43:38 +0100

    Add profile when user is created

--- profiles/models.py
+++ profiles/models.py
@@ -1,9 +1,13 @@
-from email.mime import image
 from django.db import models
+from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
+    """
+    Profile model for user profiles.
+    """
+    
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -17,3 +21,14 @@ class Profile(models.Model):
         
     def __str__(self):
         return f"{self.owner}'s profile"
+
+
+def create_profile(sender, instance, created, **kwargs):
+    """
+    Create a new profile for the user when they are created.
+    """
+    
+    if created:
+        Profile.objects.create(owner=instance)
+
+post_save.connect(create_profile, sender=User)
