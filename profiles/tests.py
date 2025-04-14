56ce9c09da6e26138e0c70d83534a451a57d097a
almost-good <marja.marinic@gmail.com>
2025-04-14 23:32:34 +0100

    Test profiles are listed correctly

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Profile
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class ProfileListViewTests(APITestCase):
+    def setUp(self):
+        User.objects.create_user(
+            username='testuser',
+            password='testpassword'
+        )
+        
+    def test_can_list_profiles(self):
+        """
+        Ensure profiles are listed correctly.
+        """
+        
+        url = '/profiles/'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
