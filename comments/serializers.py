00d7918fdef7b15ca28f396b0309fb1a6654fd3b
almost-good <marja.marinic@gmail.com>
2025-04-15 17:59:41 +0100

    Add Comment serializer

---
+++ comments/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+from .models import Comment
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Comment model.
+    """
+    
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    profile_flair = serializers.ReadOnlyField(source='owner.profile.flair')
+    
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+    
+    class Meta:
+        model = Comment
+        fields = [
+            'id',
+            'owner',
+            'is_owner',
+            'profile_id',
+            'profile_image',
+            'profile_flair',
+            'created_at',
+            'updated_at',
+            'content',
+        ]
