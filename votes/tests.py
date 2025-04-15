b50a016f7e2ce7900b1806710d660d90fa1efca7
almost-good <marja.marinic@gmail.com>
2025-04-15 22:47:01 +0100

    Test votes can be listed

--- votes/tests.py
+++ votes/tests.py
@@ -1,3 +1,46 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from posts.models import Post
+from comments.models import Comment
+from .models import Vote
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class VoteListViewTests(APITestCase):
+    def setUp(self):
+        user_one = User.objects.create_user(
+            username='testuser_one',
+            password='testpassword'
+        )
+        user_two = User.objects.create_user(
+            username='testuser_two',
+            password='testpassword'
+        )
+        
+        post = Post.objects.create(
+            owner=user_one,
+            title='Test Post owned by User One',
+        )
+        comment = Comment.objects.create(
+            owner=user_one,
+            post=post,
+            content='Test Comment owned by User One',
+        )
+        
+        post_ct = ContentType.objects.get_for_model(Post)
+        Vote.objects.create(
+            owner=user_one,
+            content_type=post_ct,
+            object_id=post.id, # type: ignore
+            value=1
+        )
+        
+    def test_can_list_votes(self):
+        """
+        Ensure votes are listed correctly.
+        """
+        
+        url = '/votes/'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
