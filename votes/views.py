ee9890f8a32278a5ada90a26f51481627c97d75d
almost-good <marja.marinic@gmail.com>
2025-04-15 21:22:46 +0100

    Add VoteList view

--- votes/views.py
+++ votes/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from memer_drf.permissions import IsOwnerOrReadOnly
+from votes.models import Vote
+from votes.serializers import VoteSerializer
 
-# Create your views here.
+
+class VoteList(generics.ListCreateAPIView):
+    """
+    List all votes or create a new vote.
+    """
+    
+    serializer_class = VoteSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Vote.objects.all()
+    
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
