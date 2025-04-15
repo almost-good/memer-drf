e1c0a9cd1caf0542d64a7cd76c8103624da3d2f0
almost-good <marja.marinic@gmail.com>
2025-04-15 22:20:30 +0100

    Remove VoteDetail view and Add Vote smart toggle option (create/update/delete)

--- votes/urls.py
+++ votes/urls.py
@@ -4,5 +4,4 @@ from votes import views
 
 urlpatterns = [
     path('votes/', views.VoteList.as_view()),
-    path('votes/<int:pk>/', views.VoteDetail.as_view()),
 ]
--- votes/views.py
+++ votes/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
 from memer_drf.permissions import IsOwnerOrReadOnly
 from votes.models import Vote
 from votes.serializers import VoteSerializer
@@ -6,7 +7,7 @@ from votes.serializers import VoteSerializer
 
 class VoteList(generics.ListCreateAPIView):
     """
-    List all votes or create a new vote.
+    List all votes and toggle a vote (smart create/delete/update)
     """
     
     serializer_class = VoteSerializer
@@ -16,12 +17,42 @@ class VoteList(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
+    def create(self, request, *args, **kwargs):
+        """
+        Create a smart toggle for votes:
+        
+            - Vote already exists: update it.
+            - Vote does not exist: create it.
+        -   Vote value is the same: delete it.
+        """
+        
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        data = serializer.validated_data
 
-class VoteDetail(generics.RetrieveUpdateDestroyAPIView):
-    """
-    Retrieve, update or delete a vote instance.
-    """
-    
-    serializer_class = VoteSerializer
-    permission_classes = [IsOwnerOrReadOnly]
-    queryset = Vote.objects.all()
+        vote, created = Vote.objects.get_or_create(
+            owner=request.user,
+            content_type=data['content_type'],
+            object_id=data['object_id'],
+            defaults={'value': data['value']}
+        )
+
+        if not created:
+            if vote.value == data['value']:
+                vote.delete()
+                return Response(
+                    {'detail': 'Vote removed.'}, 
+                    status=status.HTTP_204_NO_CONTENT
+                )
+            else:
+                vote.value = data['value']
+                vote.save()
+                return Response(
+                    self.get_serializer(vote).data, 
+                    status=status.HTTP_200_OK
+                )
+
+        return Response(
+            self.get_serializer(vote).data, 
+            status=status.HTTP_201_CREATED
+        )
