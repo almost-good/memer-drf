c6af219096bfccf04cf74e5817fbcb1657b0b183
almost-good <marja.marinic@gmail.com>
2025-04-15 21:04:09 +0100

    Add Vote Serializer

---
+++ votes/serializers.py
@@ -0,0 +1,39 @@
+from rest_framework import serializers
+from django.contrib.contenttypes.models import ContentType
+from .models import Vote
+from posts.models import Post
+from comments.models import Comment
+
+
+class VoteSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Vote model.
+    """
+    
+    owner = serializers.ReadOnlyField(source='owner.username')
+    
+    class Meta:
+        model = Vote
+        fields = [
+            'id',
+            'owner',
+            'content_type',
+            'object_id',
+            'value',
+            'created_at',
+        ]
+    
+    def validate(self, data):
+        """
+        Validate that the content type is either Post or Comment.
+        """
+        
+        allowed_models = [Post, Comment]
+        allowed_cts = [
+            ContentType.objects.get_for_model(model) for model in allowed_models
+        ]
+        if data['content_type'] not in allowed_cts:
+            raise serializers.ValidationError(
+                "Votes can only be cast on posts or comments."
+            )
+        return data
