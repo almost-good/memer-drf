04e56e78a661b7e2d8fc66a0e7b830e0d929f80f
almost-good <marja.marinic@gmail.com>
2025-04-16 20:28:32 +0100

    Add sorting filters to profiles

--- profiles/views.py
+++ profiles/views.py
@@ -18,7 +18,14 @@ class ProfileList(generics.ListCreateAPIView):
         followers_count=Count('owner__followed', distinct=True),
         following_count=Count('owner__following', distinct=True),
     ).order_by('-created_at')
-
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = [
+        'posts_count', 
+        'followers_count', 
+        'following_count',
+        'owner__following__created_at',
+        'owner__followed__created_at',
+    ]
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
     """
@@ -27,5 +34,9 @@ class ProfileDetail(generics.RetrieveUpdateAPIView):
 
     serializer_class = ProfileSerializer
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True),
+    ).order_by('-created_at')
 
