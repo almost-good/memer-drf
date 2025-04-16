b0114c591202f1fc623397daed572a4a0ca9832e
almost-good <marja.marinic@gmail.com>
2025-04-16 20:09:51 +0100

    Add vote data to comments through serializer

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,5 +1,7 @@
+from django.contrib.contenttypes.models import ContentType
 from rest_framework import serializers
 from .models import Comment
+from votes.models import Vote
 
 
 class CommentSerializer(serializers.ModelSerializer):
@@ -12,10 +14,30 @@ class CommentSerializer(serializers.ModelSerializer):
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
     profile_flair = serializers.ReadOnlyField(source='owner.profile.flair')
+    vote_id = serializers.SerializerMethodField()
+    vote_value = serializers.SerializerMethodField()
     
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+    
+    def _get_user_vote(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            post_ct = ContentType.objects.get_for_model(Comment)
+            vote = Vote.objects.filter(
+                owner=user, content_type=post_ct, object_id=obj.id
+            ).first()
+            return vote
+        return None
+    
+    def get_vote_id(self, obj):
+        vote = self._get_user_vote(obj)
+        return vote.id if vote else None # type: ignore
+    
+    def get_vote_value(self, obj):  
+        vote = self._get_user_vote(obj)
+        return vote.value if vote else None
     
     class Meta:
         model = Comment
@@ -30,6 +52,8 @@ class CommentSerializer(serializers.ModelSerializer):
             'created_at',
             'updated_at',
             'content',
+            'vote_id',
+            'vote_value',
         ]
 
 
